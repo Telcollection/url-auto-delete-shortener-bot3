e312ff1cbe9c1960adbb709dd11ca6405e990b5c
Telcollection <96669900+Telcollection@users.noreply.github.com>
2023-03-06 08:39:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jegaa1/url-auto-delete-shortener-bot3.git /url-auto-delete-shortener-bot3
+  git clone https://github.com/Telcollection/url-auto-delete-shortener-bot3.git /url-auto-delete-shortener-bot3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot3
