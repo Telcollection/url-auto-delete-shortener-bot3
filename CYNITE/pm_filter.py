be8ed574752f315ddfe4a4f2af559c53ce63ab3b
Telcollection <96669900+Telcollection@users.noreply.github.com>
2023-03-06 09:16:49 +0530

    Update pm_filter.py

--- CYNITE/pm_filter.py
+++ CYNITE/pm_filter.py
@@ -520,7 +520,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
                   ],[
-                    InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ', url="https://t.me/JAsuranserials")
+                    InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ', url="https://t.me/+awyMHD2-UZo4NzNh")
                   ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -555,7 +555,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('ʀᴇᴘᴏʀᴛ ʙᴜɢs & ꜰᴇᴇᴅʙᴀᴄᴋ', url=GRP_LNK)
         ],[
-            InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url="https://t.me/Asuranmoviefinder1")
+            InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url="https://t.me/+awyMHD2-UZo4NzNh")
         ],[
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_data')
@@ -718,10 +718,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
     elif query.data == "cynite_cnl":
             cnlbtn = [[
-                      InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='t.me/Asuranmoviefinder1')
+                      InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/+awyMHD2-UZo4NzNh')
                      ], [
-                      InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='t.me/JAsuranserials'),
-                      InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='t.me/Asuranmoviefinder1')
+                      InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='t.me/moonshows'),
+                      InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='t.me/+awyMHD2-UZo4NzNh')
                      ], [
                       InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start")
                      ]]
